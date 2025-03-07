c5774763f31a173f7e580146a643ad0b9dee9aba
robertlabrie <robert.labrie@gmail.com>
2025-03-06 20:13:27 -0500

    Add PvP Cyrodiil support (#3)
    * copy campaign code from telvar saver
    
    * bump version, API
    
    * fix api version
    
    ---------
    
    Co-authored-by: Robert Labrie <robertlabrie@gmail.com>

--- Handler.lua
+++ Handler.lua
@@ -5,8 +5,15 @@ AutoQueue.Handler = {}
 local Handler = AutoQueue.Handler
 local Settings = AutoQueue.Settings
 
+function AutoQueue.GetGroupQueue()
+  local groupQueue = false
+  if (IsUnitGrouped('player') == true) and (IsUnitGroupLeader("player") == true) then groupQueue = TVS.SV.GroupQueue end
+  return groupQueue
+end
+
 function Handler.Initialize()
   EVENT_MANAGER:RegisterForEvent(AutoQueue.name, EVENT_ACTIVITY_FINDER_STATUS_UPDATE, Handler.AutoAcceptActivity)
+  EVENT_MANAGER:RegisterForEvent(AutoQueue.name, EVENT_CAMPAIGN_QUEUE_STATE_CHANGED,Handler.AutoAcceptPVP)
 end
 
 function Handler.AutoAcceptActivity(eventCode, status)
@@ -26,5 +33,26 @@ function Handler.AutoAcceptActivity(eventCode, status)
       return
     end
     AcceptLFGReadyCheckNotification()
+  end
+end
+
+function Handler.GetGroupQueue()
+  local groupQueue = false
+  -- if (IsUnitGrouped('player') == true) and (IsUnitGroupLeader("player") == true) then groupQueue = true end -- lifted from TelVar Saver
+  -- accept queue as group only if in a group, not if lead like TVS did
+  if (IsUnitGrouped('player') == true) then groupQueue = true end -- lifted from TelVar Saver
+  return groupQueue
+end
+
+function Handler.AutoAcceptPVP(eventCode, id, isGroup, state)
+  local groupQueue = Handler.GetGroupQueue()
+  if (state == CAMPAIGN_QUEUE_REQUEST_STATE_CONFIRMING) then
+      if not Settings.vars.autoQueue then
+        if not Settings.vars.muted then
+          d('This queue was not automatically accepted. Turn it off with /autoqueue.')
+        end
+        return
+      end
+      ConfirmCampaignEntry(id, groupQueue, true)
   end
 end
